965d32026a92bb7cb322c5b7eb6a8869555f7ba9
Peemyeet <tawanweeraphan@gmail.com>
2024-12-17 19:17:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let menuIcon = document.querySelector('#menu-icon');
+
+let section = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.screenY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(link => {
+                link.classList.remove(`active`);
+                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+            })
+        }
+    })
+}
+
+
+menuIcon.onclick = (function () {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle(`active`);
+})
